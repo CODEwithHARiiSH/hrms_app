3ef425d626cb53721790c614beddda65062cdb19
harish <harishthampyhat@gmail.com>
2023-12-16 18:19:46 +0530

    Adds home page

---
+++ src/components/Home/Home.js
@@ -0,0 +1,19 @@
+
+function Home() {
+
+    return (
+        <div>
+        <br/>
+        <div className="row">
+          <div className="col-6">
+            </div>
+            <div className="col-6">
+               <h1>Home</h1> 
+               <hr/>
+            </div>
+            </div>
+            </div>
+    )
+    }
+
+  export default Home;
