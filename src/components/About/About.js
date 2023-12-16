346a47115d1dae38976b040ecf25e8967da79217
harish <harishthampyhat@gmail.com>
2023-12-16 18:20:17 +0530

    Adds About page

---
+++ src/components/About/About.js
@@ -0,0 +1,18 @@
+
+function About() {
+    return (
+        <div>
+        <br/>
+        <div className="row">
+          <div className="col-6">
+            </div>
+            <div className="col-6">
+               <h1>About</h1> 
+               <hr/>
+            </div>
+            </div>
+            </div>
+    )
+  }
+
+  export default About;
