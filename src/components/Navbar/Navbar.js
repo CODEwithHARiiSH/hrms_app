31aa90c7a4de34c06a80a3a91ef893e3c281d556
harish <harishthampyhat@gmail.com>
2023-12-16 18:41:56 +0530

    Adds function for navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar as BootstrapNavbar, Nav, NavDropdown } from 'react-bootstrap';
+
+const Navbar = ({ isLoggedIn, handleLogout }) => {
+  return (
+    <BootstrapNavbar bg="light" expand="lg">
+      <div className="container-fluid">
+        <Link to={'/'} className="navbar-brand">HRMS</Link>
+        {isLoggedIn ? (
+          <>
+            <span className="navbar-brand">Welcome</span>
+            <Nav>
+              <NavDropdown title="Employees" id="basic-nav-dropdown">
+                <NavDropdown.Item as={Link} to="/employees">
+                  Employee List
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/registeremployee">
+                  Add Employee
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <button onClick={handleLogout} className="navbar-brand">Logout</button>
+          </>
+        ) : (
+          <>
+            <Link to={"/login"} className="navbar-brand">Login</Link>
+            <Link to={"/register"} className="navbar-brand">Register</Link>
+          </>
+        )}
+        <Link to={"/about"} className="navbar-brand">About</Link>
+      </div>
+    </BootstrapNavbar>
+  );
+};
+
+export default Navbar;
